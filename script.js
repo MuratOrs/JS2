b1b6fea091c88d7d5e7bea9363d1c53926848ac8
Murat <root@Mac-Pro-Murat.local>
2019-09-28 18:48:35 +0200

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var a = 5;
+
+console.log(a);
+
+
+//let person = {
+//    name: 'Peter',
+//    age: 32,
+//    auto: 'Ford',
+//    isMarried: false
+//}
+//
+//console.log(person['name']);
+//
+//let arr = ['plum.png', 'orange.jpg', 'apple.bmp']
+//
+//console.log(arr[0 ]);
+//alert('Hello, world!');
+//var conf = +prompt('Вы уже совершеннолетний?', +18)
+//console.log(conf);
+
+let incr = 10,
+    decr = 10;
+
+    console.log(incr++);
+    console.log(decr--);
+    console.log(5%4);    
+    console.log(3**6);
+
+    let isCheked = false,
+    isClose = false;
+
+    console.log(isCheked || isClose);
+    
