f5a6b48c23889043f0ab126b438fae0822f96fa3
Murat <root@Mac-Pro-Murat.local>
2019-09-29 16:49:46 +0200

    Новый документ

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
-'use strict';
-
-var a = 5;
-
-console.log(a);
-
-
-//let person = {
-//    name: 'Peter',
-//    age: 32,
-//    auto: 'Ford',
-//    isMarried: false
-//}
-//
-//console.log(person['name']);
-//
-//let arr = ['plum.png', 'orange.jpg', 'apple.bmp']
-//
-//console.log(arr[0 ]);
-//alert('Hello, world!');
-//var conf = +prompt('Вы уже совершеннолетний?', +18)
-//console.log(conf);
-
-let incr = 10,
-    decr = 10;
-
-    console.log(incr++);
-    console.log(decr--);
-    console.log(5%4);    
-    console.log(3**6);
-
-    let isCheked = false,
-    isClose = false;
-
-    console.log(isCheked || isClose);
+let num = 49;
+if (num < 49){
+    console.log('Это не верно!');
+} else if (num>100 && num <300) {
+    console.log('Больше 100');
     
+} else if (num<100) {
+    console.log('Меньше ста');
+    
+} else{
+    console.log('Какая-то шняга');
+    
+}
+
+(num == 49)? console.log('Верно'): console.log('Неверно');
+
