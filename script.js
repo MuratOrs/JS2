a3d1033270d50689734343bfad3eee1b3954c1be
Murat <root@Mac-Pro-Murat.local>
2019-10-02 18:29:07 +0200

    Изучаем Функции

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-/* let a = 9;
-if (a !== 9){
-    console.log('Добро пожаловать');    
-} else {
-    console.log('Доступ запрещен');
-}
+/* 'use strict';
+let num = 20;
+
+function showFirstMessage(text){
+    alert(text);
+    console.log(num);
+    
+ }
+
+showFirstMessage('Hello, world!');
+console.log(num);
  */
 
- const button = document.querySelector('button');
- const input = document.querySelector('.age')
-// () => стрелочная функция
- button.onclick = () => {
- let num = +input.value;    
- if (num >=16 && num <80){
-     console.log('Добро пожаловать');
-     
- } else if (num >= 100 && num < 140) {
-     console.log('Вам позновато!');
-   } else {
-console.log('Хватит прикалываться! Я вычислю вас по ай пи');
+//Функия работает до объявления в коде
 
-   }
- }
+let calc = (a,b) => a*b;
+
+
+console.log(calc(3,4));
+console.log(calc(4,2));
+
+
+function retVar(){
+    let num = 50;
+    return num;
+}
+
+let anotherNum = retVar();
+console.log(anotherNum);
+
+let str = 'Test';
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let hor = "15.3px";
+//console.log(Math.round(hor));
+console.log(parseInt(hor));
+console.log(parseFloat(hor));
